633fb00dcf12c0a4731ad428453988e54a20b494
vivek rawat <rawatvivek239@gmail.com>
2021-06-13 12:10:52 +0530

    added show page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router';
 import './App.css';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
+import Show from './pages/Show';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
       </Route>
       <Route exact path="/starred">
         <Starred />
+      </Route>
+      <Route exact path="/show/:id">
+        <Show />
       </Route>
       <Route>There is no page with this url</Route>
     </Switch>
---
+++ src/pages/Show.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { apiGet } from '../misc/config';
+
+const Show = () => {
+  const { id } = useParams();
+
+  const [show, setShow] = useState(null);
+
+  useEffect(() => {
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(r => setShow(r));
+  }, [id]);
+
+  console.log(show);
+
+  return <div>X</div>;
+};
+
+export default Show;
